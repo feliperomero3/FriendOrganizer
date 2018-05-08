deffa83a11e72ba4bdb94a7f3a8ac95ac9b9ea93
Felipe Romero <feliperomero3@outlook.com>
2018-05-08 17:42:19 -0600

    Crear MainViewModel.

---
+++ FriendOrganizer.UI/Data/IFriendDataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Domain;
+
+namespace FriendOrganizer.UI.Data
+{
+    public interface IFriendDataService
+    {
+        IEnumerable<Friend> GetAll();
+    }
+}
